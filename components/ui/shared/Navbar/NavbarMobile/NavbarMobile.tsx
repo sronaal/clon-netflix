c1bf2a517c969206dcfb5d7b24b86d69a12981c2
Ronal <ronal.sanx21@gmail.com>
2024-12-22 18:35:53 -0500

    Navbar mobile

--- components/ui/shared/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/ui/shared/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { BellRing, Menu, Search } from "lucide-react"
+import Link from "next/link"
+import { itemsNavbar } from "@/data/itemsNavbar"
+import { Logo } from '@/components/ui/shared/Logo/Logo'
+
+
 
 export default function NavbarMobile() {
-  return (
-    <>
-        
-    </>
-  )
+    return (
+        <>
+            <div className='p-4 flex justify-between'>
+                <Logo />
+                <Sheet>
+                    <SheetTrigger>
+                        <Menu/>
+                    </SheetTrigger>
+                    <SheetContent side="left" className="bg-black">
+                       <div className="flex flex-col gap-4">
+                       {
+                        itemsNavbar.map((item) => (
+                            <Link key={item.name} href={item.link} className='hover:text-gray-300 transition-all duration-300'>
+                                {item.name}
+                            </Link> 
+                        ))
+                       }
+                       </div>
+
+                       <div className="border-[1px] border-white/70 my-5"/>
+                        <div className="flex justify-between gap-6 mt-4">
+                            <Search className="cursor-pointer"/>
+                            <BellRing className="cursor-pointer"/>
+                            <p>User</p>
+                        </div>
+
+                    </SheetContent>
+                </Sheet>
+
+            </div>
+        </>
+    )
 }
