32190043432d5fbd34f851cf70359e7f71209cf7
Ronal <ronal.sanx21@gmail.com>
2024-12-22 19:26:03 -0500

    Botones Slider Video

--- app/(routes)/(home)/components/SliderVideo.tsx
+++ app/(routes)/(home)/components/SliderVideo.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import { Info, Play } from 'lucide-react'
 import React from 'react'
 
 export const SliderVideo = () => {
@@ -13,9 +15,24 @@ export const SliderVideo = () => {
         <div className='flex flex-col justify-end absolute w-full md:w-[36%] px-4 md:px-0 md:left-[4%] 
         z-20 top-0 -bottom-7 md:bottom-[36%]'>
             <div className='pt-24 md:pt-0'>
-                <h2 className='text-2xl md:text-5xl lg:text-8xl font-bold drop-shadow-xl'>
+                <h2 className='text-2xl md:text-2xl lg:text-4xl font-bold drop-shadow-xl'>
                     Clon de Netlix
                 </h2>
+                <p className='max-w-md mt-2 text-xs  md:text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nostrum enim minima labore aliquam voluptates doloremque consequatur atque, nobis voluptate? Eaque nisi veniam ea autem minus explicabo eius hic quidem!</p>
+                
+                <div className='flex flex-col md:flex-row gap-4 mt-4'>
+                    <Button size="lg" variant="secondary">
+                        <Play className='h-6 w-6 mr-2 fill-black'/>
+                        Reproducir
+                    </Button>
+                    <Button size="lg" variant="secondary" className='bg-gray-600 hover:bg-gray-500/40'>
+                        <Info className='h-6 w-6 mr-2'/>
+                        Más información
+                    </Button>
+
+
+
+                </div>
             </div>
         </div>
     </div>
