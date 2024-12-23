6b33410038daa1e5097d9f1c2b94416b750f906f
Ronal <ronal.sanx21@gmail.com>
2024-12-23 12:27:13 -0500

    Formulario

--- app/(routes)/(auth)/login/LoginForm/LoginForm.tsx
+++ app/(routes)/(auth)/login/LoginForm/LoginForm.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+"use client"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+})
 
 export default function LoginForm() {
-  return (
-    <div>LoginForm</div>
-  )
+
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    })
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type='password' placeholder="password" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your private password.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
 }
